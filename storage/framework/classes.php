e03d82d118345ac81860e22cd6377e90b4f73a65
Nam Dinh <namdwb@gmail.com>
2019-06-23 14:31:13 +0700

    init

---
+++ storage/framework/classes.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
+  'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+  'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+  'cms\\Controllers\\index' => 'modules\\cms\\controllers\\index.php',
+  'backend\\Controllers\\media' => 'modules\\backend\\controllers\\media.php',
+);
